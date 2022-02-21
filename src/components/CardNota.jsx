467848db8aa3a360f5ce80d8f3787801fae2ca9b
cristian.etchebest <cristian.etchebest@meta.com.br>
2022-02-21 14:38:17 -0300

    Componentizando CEEP utilizando class

---
+++ src/components/CardNota.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+class CardNota extends Component {
+  render() {
+    return (
+      <section>
+        <header>
+          <h3>Título</h3>
+        </header>
+        <p>Escreva aqui</p>
+      </section>
+    );
+  }
+}
+export default CardNota;
