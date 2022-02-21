eceb6df73c801815fe2b140ba0147f6cb6bc456b
cristian.etchebest <cristian.etchebest@meta.com.br>
2022-02-21 18:20:28 -0300

    Incluindo imagem SVG via img ou ReactComponente

--- src/components/CardNota/CardNota.jsx
+++ src/components/CardNota/CardNota.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import "./CardNota.css";
+import deleteSVG from "../../assets/image/delete.svg";
+// import {ReactComponent as DeleteSVG} from "../../assets/image/delete.svg";
 
 class CardNota extends Component {
   render() {
@@ -7,6 +9,8 @@ class CardNota extends Component {
       <section className="card-nota">
         <header className="card-nota_cabecalho">
           <h3 className="card-nota_titulo">{this.props.titulo}</h3>
+          <img src={deleteSVG} alt="Excluir" />
+          {/* <DeleteSVG/> */}
         </header>
         <p className="card-nota_texto">{this.props.texto}</p>
       </section>
